70e9307894127f35d14f9123224a745af4ef47dd
crascot <y-sitdikov@mail.ru>
2021-03-26 22:46:59 +0600

    block props

--- src/components/skills/examples/block/block.jsx
+++ src/components/skills/examples/block/block.jsx
@@ -4,14 +4,14 @@ import { Card, Button } from 'react-bootstrap';
 function Block(props) {
     return (
         <Card style={{ width: '18rem' }}>
-            <Card.Img variant="top" src={props.image} />
+            <Card.Img variant="top" src={props.classes.image} />
             <Card.Body>
-                <Card.Title>{props.name}</Card.Title>
+                <Card.Title>{props.classes.name}</Card.Title>
                 <Card.Text>
                     <ul>
-                        <li>Адаптивность: {props.info}</li>
-                        <li>SCSS: {props.info}</li>
-                        <li>Bootstrap: {props.info}</li>
+                        <li>Адаптивность: {props.classes.info[0]}</li>
+                        <li>SCSS: {props.classes.info[1]}</li>
+                        <li>Bootstrap: {props.classes.info[2]}</li>
                     </ul>
                 </Card.Text>
                 <Button variant="primary" size="xs" block href="https://crascot.github.io/porten/">Ссылка на данный сайт</Button>
