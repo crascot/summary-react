4c89938967b59e080f83cd2d737a2b938fe83c91
crascot <y-sitdikov@mail.ru>
2021-03-29 22:23:27 +0600

    css modules

--- src/components/head/head.js
+++ src/components/head/head.js
@@ -2,13 +2,13 @@ import React from 'react';
 import { Image } from 'react-bootstrap';
 import Info from './info/info';
 import face from './face.jpg';
-import './head.css';
+import style from './head.module.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function Head() {
     return (
-        <div className='head'>
-            <Image className='face' src={face} roundedCircle />
+        <div className={ style.head }>
+            <Image className={ style.face } src={face} roundedCircle />
             <Info />
         </div>
     )
--- src/components/head/info/info.jsx
+++ src/components/head/info/info.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
-import './info.css';
+import style from './info.module.css';
 
 
 function Info() {
     return (
-        <div className='info'>
+        <div className={ style.info }>
             <h1>Ситдиков Ярослав</h1>
             <p>Я могу легко влиться в коллектив и связанно работать в команде.
             Давно интересовался IT-индустрией, но всерьез начал обучаться этому в 2018 году.
             Пробовал разные специальности но больше всего получилось развить Frontend.
         </p>
-            <div className="links">
+            <div className={ style.links }>
                 <a href="https://t.me/crascot">Telegram</a>
                 <a href="https://join.skype.com/invite/jw2FSUrQojO5">Skype</a>
                 <a href="https://github.com/crascot">Github</a>
