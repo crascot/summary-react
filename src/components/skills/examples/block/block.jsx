bb54e965fcbe9ee4906f1067e9d08f33cb24952d
crascot <y-sitdikov@mail.ru>
2021-04-05 22:12:47 +0600

    array

--- src/components/skills/examples/block/block.jsx
+++ src/components/skills/examples/block/block.jsx
@@ -4,17 +4,18 @@ import { Card, Button } from 'react-bootstrap';
 function Block(props) {
     return (
         <Card style={{ width: '18rem' }}>
-            <Card.Img variant="top" src={props.classes.image} />
+            <Card.Img variant="top" src={props.image} />
             <Card.Body>
-                <Card.Title>{props.classes.name}</Card.Title>
+                <Card.Title>{props.name}</Card.Title>
                 <Card.Text>
                     <ul>
-                        <li>Адаптивность: {props.classes.info[0]}</li>
-                        <li>SCSS: {props.classes.info[1]}</li>
-                        <li>Bootstrap: {props.classes.info[2]}</li>
+                        <li>Адаптивность: {props.info[0]}</li>
+                        <li>SCSS: {props.info[1]}</li>
+                        <li>Bootstrap: {props.info[2]}</li>
                     </ul>
                 </Card.Text>
-                <Button variant="primary" size="xs" block href={props.classes.ref}>Ссылка на данный сайт</Button>
+                <Button variant="primary" size="xs" block href={props.reference}>Ссылка на данный сайт</Button>
+            
             </Card.Body>
         </Card>
     )
